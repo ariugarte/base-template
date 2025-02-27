c73228036c727a7f11f8f41e19d56c24a1f78aaa
ari ugarte <ari.ugarte.gcp@gmail.com>
2025-02-27 15:07:54 +0100

    new files

---
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/src/{{cookiecutter.project_slug}}/__init__.py
@@ -0,0 +1,10 @@
+"""
+{{ cookiecutter.project_name }}
+"""
+
+from {{ cookiecutter.project_slug }}.__about__ import __application__, __version__
+
+__all__ = [
+    "__application__",
+    "__version__",
+]
